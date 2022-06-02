1867cb28249faf385c5195e77fe1fe1e522d3c8a
Amosoneh <amoskhaled@gmail.com>
2022-06-02 12:18:45 +0100

    update

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+x_int = 0
+while x_int < 10:
+    print(x_int, end=' ')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    x_int += 1
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print()
+print("Final value of x_int: ", x_int)
