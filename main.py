6b07639fe8c4f80c2ad3e15061ed4dab4824c6b6
Oneh Amos <amoskhaled@gmail.com>
2022-06-05 12:46:53 +0100

    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 x_int = 0
 while x_int < 10:
     print(x_int, end=' ')
@@ -6,3 +7,21 @@ while x_int < 10:
 
 print()
 print("Final value of x_int: ", x_int)
+=======
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+>>>>>>> c3e3075 (commit)
